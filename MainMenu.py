70963db3eedb37d751adada86e300d8c8d44596d
Mohit-d13 <mohitdamle14@gmail.com>
2025-01-12 21:30:21 +0530

    Updated MainMenu module
    added error handling of python like try...except block to concise code and avoid unwanted error

--- MainMenu.py
+++ MainMenu.py
@@ -9,22 +9,25 @@ def Adminmenu() :
         print("4. User Feedback and Ratings Table")
         print("5. Logout ")
         print("===============================================================")
-        choice=int(input("Enter Choice between 1 to 5-------> : "))
-        if choice==1:
-            Operations.BookManagement()
-        elif choice==2:
-            Operations.UserManagement()
-        elif choice==3:
-            Operations.AdminManagement()
-        elif choice==4:
-            Operations.FeedbackTable()
-        elif choice==5:
-            print("Thanks for visiting our Library:))")
-            print("Logged out of the system")
-            break
-        else:
-            print("Wrong Choice......Enter Your Choice again")
-            continue
+        try:
+            choice=int(input("Enter Choice between 1 to 5-------> : "))
+            if 1 <= choice <= 5:
+                if choice==1:
+                    Operations.BookManagement()
+                elif choice==2:
+                    Operations.UserManagement()
+                elif choice==3:
+                    Operations.AdminManagement()
+                elif choice==4:
+                    Operations.FeedbackTable()
+                elif choice==5:
+                    print("Thanks for visiting our Library:))")
+                    print("Logged out of the system")
+                    break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid Input. Please enter a number between 1 and 5.")
     
 
 def Usermenu() :
@@ -35,16 +38,21 @@ def Usermenu() :
         print("2. Feedback and Ratings")
         print("3. Logout ")
         print("===============================================================")
-        choice=int(input("Enter Choice between 1 to 3-------> : "))
-        if choice==1:
-            Operations.BookCentre()
-        elif choice==2:
-            Operations.Feedback()
-        elif choice==3:
-            print("Thanks for visiting our Library:))")
-            print("Logged out of the system")
-            break
-        else:
-            print("Wrong Choice......Enter Your Choice again")
-            continue
+        try:
+            choice=int(input("Enter Choice between 1 to 3-------> : "))
+            if 1 <= choice <= 3:
+                if choice==1:
+                    Operations.BookCentre()
+                elif choice==2:
+                    Operations.Feedback()
+                elif choice==3:
+                    print("Thanks for visiting our Library:))")
+                    print("Logged out of the system")
+                    break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid Input. Please enter a number between 1 and 3")
+            
+            
    
